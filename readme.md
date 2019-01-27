ae93b35ff4012fbf78a61e4f99f3e068be661870
BuildTools <unwarned@unknown.spigot.mc>
2019-01-27 18:53:47 +0200

    #5 Added desktop objects management
    - added desktop objects management (This PC, Rubbish Bin, Control Panel,
    Network, User Folder)
    - fixed some bugs
    - redesigned interface

--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 Small console tweaker for Windows 10 with some useful options
 
 Main features:
+- add or remove desktop objects (This PC, Recycle Bin etc)
 - control Windows Update Center
 - control suggestions and auto completion
 - setup Microsoft Office Professional Plus 2016
 
-<!-- - add "This PC" shortcut on the desktop -->
 <!-- - restore SPPSvc -->
--- tenTweaker.cmd
+++ tenTweaker.cmd
@@ -20,12 +20,7 @@ echo.^(^!^) The author is not responsible for any possible damage to the compute
 echo.^(^?^) Are you sure^? ^(Enter or close^)
 pause>nul
 
-
-
-
-
 goto :mainMenu
-rem reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f
 
 
 
@@ -59,22 +54,26 @@ exit
 
 
 :mainMenu
-set errorLevel=
 call :logo
+echo.^(^i^) Main Menu
+echo.
+echo.
 echo.^(^>^) Choose action:
-echo.    ^(1^) Control Windows Update
+echo.    ^(1^) Add or remove desktop objects ^(This PC, Recycle Bin etc^)
 echo.    ^(2^) Control suggestions and auto completion
-echo.    ^(3^) Setup Microsoft Office Professional Plus 2016
+echo.    ^(3^) Control Windows Update
+echo.    ^(4^) Setup Microsoft Office Professional Plus 2016
 echo.
 echo.
 echo.
-choice /c 123 /n /m "> "
+choice /c 1234 /n /m "> "
+set command=%errorLevel%
 
 
-if "%errorLevel%" == "1" call :windowsUpdateControlMenu
-if "%errorLevel%" == "2" call :suggestionsControlMenu
-if "%errorLevel%" == "3" call :officeSetupMenu
-
+if "%command%" == "1" call :desktopObjectsMenu
+if "%command%" == "2" call :suggestionsControlMenu
+if "%command%" == "3" call :windowsUpdateControlMenu
+if "%command%" == "4" call :officeSetupMenu
 
 if "%errorLevel%" == "1234567890" exit /b
 goto :mainMenu
@@ -93,86 +92,63 @@ goto :mainMenu
 
 
 
-:windowsUpdateControlMenu
-set windowsUpdateState=enabled
-for /f "skip=3 tokens=1,2,3* delims= " %%i in ('sc query wuauserv') do if "%%i" == "STATE" if "%%k" == "1" set windowsUpdateState=disabled
+:desktopObjectsMenu
+set desktopObjects_thisPC=hidden
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D}') do if "%%i" == "0x0" set desktopObjects_thisPC=shown
 
-set errorLevel=
+set desktopObjects_recycleBin=hidden
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {645FF040-5081-101B-9F08-00AA002F954E}') do if "%%i" == "0x0" set desktopObjects_recycleBin=shown
+
+set desktopObjects_controlPanel=hidden
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}') do if "%%i" == "0x0" set desktopObjects_controlPanel=shown
+
+set desktopObjects_userFolder=hidden
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {59031a47-3f72-44a7-89c5-5595fe6b30ee}') do if "%%i" == "0x0" set desktopObjects_userFolder=shown
+
+set desktopObjects_network=hidden
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C}') do if "%%i" == "0x0" set desktopObjects_network=shown
 
 call :logo
-echo.^(^i^) Windows Update Control Menu
-echo.
-echo.^(^!^) Current Windows Update state: %windowsUpdateState%
+echo.^(^i^) Desktop Objects Control Menu
 echo.
 echo.
-echo.
-echo.^(^>^) Choose action:
-echo.    ^(1^) Clear Windows Update distributions
-echo.    ^(2^) Disable Windows Update Center and remove all downloaded updates
-echo.    ^(3^) Enable Windows Update Center and launch it
+echo.^(^>^) Choose action to show/hide desktop object:
+echo.    ^(1^) This PC             %desktopObjects_thisPC%
+echo.    ^(2^) Recycle Bin         %desktopObjects_recycleBin%
+echo.    ^(3^) Control Panel       %desktopObjects_controlPanel%
+echo.    ^(4^) User Folder         %desktopObjects_userFolder%
+echo.    ^(5^) Network             %desktopObjects_network%
 echo.
 echo.    ^(0^) Go back
 echo.
 echo.
 echo.
-choice /c 1230 /n /m "> "
-
-if "%errorLevel%" == "4" exit /b
-call :windowsUpdateControlAction%errorLevel%
-goto :windowsUpdateControlMenu
+choice /c 123450 /n /m "> "
+set command=%errorLevel%
 
 
+if "%command%" == "1" if "%desktopObjects_thisPC%" == "hidden" (
+  reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 0 /f
+) else reg add reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 1 /f
 
+if "%command%" == "2" if "%desktopObjects_recycleBin%" == "hidden" (
+  reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {645FF040-5081-101B-9F08-00AA002F954E} /t REG_DWORD /d 0 /f
+) else reg add reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {645FF040-5081-101B-9F08-00AA002F954E} /t REG_DWORD /d 1 /f
 
+if "%command%" == "3" if "%desktopObjects_controlPanel%" == "hidden" (
+  reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0} /t REG_DWORD /d 0 /f
+) else reg add reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0} /t REG_DWORD /d 1 /f
 
-:windowsUpdateControlAction1
-call :logo
-echo.^(^i^) Removing downloaded Windows Update Center distributions of updates...
-for /l %%i in (4,-1,1) do rd /s /q %WinDir%\SoftwareDistribution\Download
-timeout /nobreak /t 1 >nul
-exit /b
+if "%command%" == "4" if "%desktopObjects_userFolder%" == "hidden" (
+  reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {59031a47-3f72-44a7-89c5-5595fe6b30ee} /t REG_DWORD /d 0 /f
+) else reg add reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {59031a47-3f72-44a7-89c5-5595fe6b30ee} /t REG_DWORD /d 1 /f
 
+if "%command%" == "5" if "%desktopObjects_network%" == "hidden" (
+  reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 0 /f
+) else reg add reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel /v {F02C1A0D-BE21-4350-88B0-7367FC96EF3C} /t REG_DWORD /d 1 /f
 
-
-
-
-:windowsUpdateControlAction2
-call :logo
-echo.^(^i^) Terminating the Windows Update Center...
-for /l %%i in (4,-1,1) do sc stop wuauserv
-timeout /nobreak /t 1 >nul
-
-echo.^(^i^) Disabling Windows Update Center...
-for /l %%i in (4,-1,1) do sc config wuauserv start=disabled
-timeout /nobreak /t 1 >nul
-
-echo.^(^i^) Removing downloaded Windows Update Center distributions of updates...
-for /l %%i in (4,-1,1) do rd /s /q %WinDir%\SoftwareDistribution\Download
-timeout /nobreak /t 1 >nul
-
-echo.^(^i^) Closing access to downloaded Windows Update Center distributions of updates directory...
-for /l %%i in (4,-1,1) do echo.>%WinDir%\SoftwareDistribution\Download
-timeout /nobreak /t 1 >nul
-exit /b
-
-
-
-
-
-:windowsUpdateControlAction3
-call :logo
-echo.^(^i^) Enabling Windows Update Center...
-for /l %%i in (4,-1,1) do sc config wuauserv start=auto
-timeout /nobreak /t 1 >nul
-
-echo.^(^i^) Launching Windows Update Center...
-for /l %%i in (4,-1,1) do sc start wuauserv
-timeout /nobreak /t 1 >nul
-
-echo.^(^i^) Opening access to downloaded Windows Update Center distributions of updates directory...
-for /l %%i in (4,-1,1) do del /q "%WinDir%\SoftwareDistribution\Download"
-timeout /nobreak /t 1 >nul
-exit /b
+if "%command%" == "6" ( set command= & exit /b )
+goto :desktopObjectsMenu
 
 
 
@@ -189,48 +165,102 @@ exit /b
 
 
 :suggestionsControlMenu
-set suggestions_autoSuggestState=disabled
-for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v AutoSuggest') do if "%%i" == "yes" set suggestions_autoSuggestState=enabled
+set suggestions_autoSuggest=disabled
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v AutoSuggest') do if "%%i" == "yes" set suggestions_autoSuggest=enabled
 
-set suggestions_appendCompletionState=disabled
-for /f "skip=2 tokens=4,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v "Append Completion"') do if "%%i" == "yes" set suggestions_appendCompletionState=enabled
+set suggestions_appendCompletion=disabled
+for /f "skip=2 tokens=4,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v "Append Completion"') do if "%%i" == "yes" set suggestions_appendCompletion=enabled
 
-set suggestions_startTrackProgsState=disabled
-for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_TrackProgs') do if "%%i" == "0x1" set suggestions_startTrackProgsState=enabled
-
-set errorLevel=
+set suggestions_startTrackProgs=disabled
+for /f "skip=2 tokens=3,* delims= " %%i in ('reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Start_TrackProgs') do if "%%i" == "0x1" set suggestions_startTrackProgs=enabled
 
 call :logo
 echo.^(^i^) Suggestions Control Menu
 echo.
-echo.^(^!^) Auto Suggest state: %suggestions_autoSuggestState%
-echo.^(^!^) Append Completion state: %suggestions_appendCompletionState%
-echo.^(^!^) Start Track Progs state: %suggestions_startTrackProgsState%
 echo.
-echo.
-echo.
-echo.^(^>^) Choose action:
-echo.    ^(1^) Auto Suggest
-echo.    ^(2^) Append Completion
-echo.    ^(3^) Start Track Progs
+echo.^(^>^) Choose action to enable/disable suggestions:
+echo.    ^(1^) Auto Suggest            %suggestions_autoSuggest%
+echo.    ^(2^) Append Completion       %suggestions_appendCompletion%
+echo.    ^(3^) Start Track Progs       %suggestions_startTrackProgs%
 echo.
 echo.    ^(0^) Go back
 echo.
 echo.
 echo.
 choice /c 1230 /n /m "> "
+set command=%errorLevel%
 
-if "%errorLevel%" == "1" if "%suggestions_autoSuggestState%" == "disabled" (
+
+if "%command%" == "1" if "%suggestions_autoSuggest%" == "disabled" (
   reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v AutoSuggest /t REG_SZ /d yes /f
 ) else reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v AutoSuggest /t REG_SZ /d no /f
-if "%errorLevel%" == "2" if "%suggestions_appendCompletionState%" == "disabled" (
+
+if "%command%" == "2" if "%suggestions_appendCompletion%" == "disabled" (
   reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v "Append Completion" /t REG_SZ /d yes /f
 ) else reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v "Append Completion" /t REG_SZ /d no /f
-if "%errorLevel%" == "3" if "%suggestions_appendCompletionState%" == "disabled" (
+
+if "%command%" == "3" if "%suggestions_startTrackProgs%" == "disabled" (
   reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v Start_TrackProgs /t REG_SZ /d 00000001 /f
 ) else reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete /v Start_TrackProgs /t REG_SZ /d 00000000 /f
-if "%errorLevel%" == "4" exit /b
+
+if "%command%" == "4" ( set command= & exit /b )
 goto :suggestionsControlMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:windowsUpdateControlMenu
+set windowsUpdate_updateDistributions=unlocked
+for /f "delims=" %%i in ('dir /a:-d /b %WinDir%\SoftwareDistribution\Download') do if "%%i" == "Download" set windowsUpdate_updateDistributions=locked
+
+set windowsUpdate_updateCenter=enabled
+for /f "skip=3 tokens=1,2,3* delims= " %%i in ('sc query wuauserv') do if "%%i" == "STATE" if "%%k" == "1" set windowsUpdate_updateCenter=disabled
+
+call :logo
+echo.^(^i^) Windows Update Control Menu
+echo.
+echo.
+echo.^(^>^) Choose action:
+echo.    ^(1^) Windows Update distributions       %windowsUpdate_updateDistributions%
+echo.    ^(2^) Windows Update Center              %windowsUpdate_updateCenter%
+echo.
+echo.    ^(0^) Go back
+echo.
+echo.
+echo.
+choice /c 120 /n /m "> "
+set command=%errorLevel%
+
+
+if "%command%" == "1" if "%windowsUpdate_updateDistributions%" == "unlocked" (
+  for /l %%i in (4,-1,1) do rd /s /q "%WinDir%\SoftwareDistribution\Download"
+  echo.>"%WinDir%\SoftwareDistribution\Download"
+) else (
+  del /q "%WinDir%\SoftwareDistribution\Download"
+  md "%WinDir%\SoftwareDistribution\Download"
+)
+
+if "%command%" == "2" if "%windowsUpdate_updateCenter%" == "enabled" (
+  for /l %%i in (4,-1,1) do sc stop wuauserv
+  for /l %%i in (4,-1,1) do sc config wuauserv start=disabled
+) else (
+  for /l %%i in (4,-1,1) do sc config wuauserv start=auto
+  for /l %%i in (4,-1,1) do sc start wuauserv
+)
+
+if "%command%" == "3" ( set command= & exit /b )
+goto :windowsUpdateControlMenu
 
 
 
@@ -251,11 +281,8 @@ set officeSetupURL=https://onedrive.live.com/download?cid=D3AF852448CB4BF6^&resi
 set officeSetupAdditionalURL=https://public.dm.files.1drv.com/y4mTqNAebstFsw9p507h2xqKwivr_pHN6OwyaEAA3-xavLhFr_9HmsF-bF931oFmOZ-ynEy53Blug8XG1FLTmT0VT36kjGfbT1a_tItImyjwJqqKSTp1qCXBdPbKmlI5uNy0P6tkSMicg32ddWL3Z91nyoXV8SXymCpC_Bwp1SoqzBjBNAV4CXfr5t-QtlkJapj/Microsoft%%20Office%%20Professional%%20Plus%%202016.iso?access_token=EwAIA61DBAAUcSSzoTJJsy%%2bXrnQXgAKO5cj4yc8AAdNI1D0Km20nFjkwjZJAiQrksgJ3Bpa5AYk%%2fVPN9VGXuBitjIC6LhGh3WQcX%%2fE%%2f0V9IPo7%%2f2JLzjJnJ9%%2bSwX%%2bNm37S8I6zXYsDfy7AervE2iGE%%2bSJ901s1sjMHULB%%2btCGYvsUIEHNQTPA4dAn8gCmlrpp%%2f%%2f6cGuJnBlc2jysi1%%2bxKUcREdO8tfwpLvXaR9W%%2btDp5kKiLXvKuG9H0gCLpbknzFMkyaeeGemUTzGRglwqTTPlp94%%2fEmaMW9O5qg2STAFqKV6H%%2f%%2flNtevRIoCctJgU9dXcOfbc5YdRhySjbBGJxDLReJJk4X2zeRvq62G3ITD25jEOwYufL7POHXJOe47kDZgAACEMQTepMithw2AEdh0sQB%%2bLFCpxLdVafSfaeStp31%%2fHUPqg7TeINPS7DuEP3Ga%%2fqOPNX6CtkWzkrodHWyXsQj5eSV6ZMFZdZa2zrxSntXJs%%2bkaVAMLvGtXN8lwMXjyCZw8yhboCdwEqR8IzbgZsTR5DOXGLAcq%%2fRt81DQzUnnsHdnsuDO%%2ffELmE8ccu3eBp3ntqzz9MqxpsLotGpmwL5y72QWnmFM4UnCEhTYo1QzYoxyELtavpBik5y2%%2fSLUthnrXtxUGLuj9xAHcXfewJmGbhA3DVSnKdx9RqckzYjqBBISzqYQVmbWJeYsZIQaQrhcOkudEbpVTUplF4I%%2bYOJqiOCSI6W9lL6fTWdLuMYgsXTnnMtFMNPYeTTaYDQoZj1GqAZckKcdscy%%2b%%2foNZXkSNlPaJEZdZoozvuEFgRzt%%2fmWM9YvS7aCfia6kwDRxY9VEYwLvPQNhFpg3DGpTI%%2brrKokLUs6q9TIUBUfD1SUbXMTnN8cB1Jpsveic9wAfhg837RZVdBvfWZOYnv4myviNwqtXjgaxtzwpb6atb4EEOy6KQLAhqbZwHBdWIQhypIqFfRcATwpSENEP%%2b2hF7T878znu3rE%%2fJYijcuk%%2fH8GlzFi7y7y9%%2bl3hsW4L7eb6ybZD%%2fy7JEAI%%3d
 set officeSetupISO=Microsoft Office Professional Plus 2016.iso
 
-set errorLevel=
-
 call :logo
 echo.^(^i^) Microsoft Office Professional Plus 2016 Setup Menu
-echo.
 echo.
 echo.
 echo.^(^>^) Choose action:
@@ -266,17 +293,12 @@ echo.
 echo.
 echo.
 choice /c 10 /n /m "> "
-
-if "%errorLevel%" == "1" call :officeSetup
-if "%errorLevel%" == "2" exit /b
-exit /b
+set command=%errorLevel%
 
 
-
-
+if "%command%" == "2" ( set command= & exit /b )
 
 :officeSetup
-set errorLevel=
 call :logo
 if not exist "%officeSetupISO%" (
   echo.^(^i^) Downloading Microsoft Office Professional Plus 2016 Setup
@@ -290,19 +312,20 @@ if not exist "%officeSetupISO%" (
 
 echo.^(^i^) Mounting iso file...
 powershell.exe "Mount-DiskImage ""%~dp0%officeSetupISO%"""
-timeout /nobreak /t 5 >nul
+timeout /nobreak /t 1 >nul
 
 echo.^(^i^) Running setup...
-for /f "skip=3" %%i in ('powershell.exe "Get-DiskImage ""%~dp0%officeSetupISO%"" | Get-Volume | Select-Object {$_.DriveLetter}"') do start /wait %%i:\O16Setup.exe
+for /f "skip=3" %%i in ('powershell.exe "Get-DiskImage """%~dp0%officeSetupISO%""" | Get-Volume | Select-Object {$_.DriveLetter}"') do start /wait %%i:\O16Setup.exe
+timeout /nobreak /t 1 >nul
 
 choice /c yn /n /m "(>) Setup is completed? (y/n) > "
-if "%errorLevel%" == "2" goto :officeSetup
-set errorLevel=
+set command=%errorLevel%
+if "%command%" == "2" goto :officeSetup
 
 echo.^(^i^) Unmounting iso file...
 powershell.exe "Dismount-DiskImage ""%~dp0%officeSetupISO%"""
 timeout /nobreak /t 1 >nul
-exit /b
+goto :officeSetupMenu
 
 
 
@@ -325,7 +348,7 @@ cls
 echo.
 echo.
 echo.    [MikronT] ==^> Ten Tweaker
-echo.   =====================================
+echo.   ===========================
 echo.     See other here:
 echo.         github.com/MikronT
 echo.
