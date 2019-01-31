4c267b20bb124ad58e6faa764c78b803f0e7dafd
BuildTools <unwarned@unknown.spigot.mc>
2019-01-31 16:10:11 +0200

    #10 Added gpedit.msc activation i Windows 10 Home
    - added gpedit.msc activation i Windows 10 Home
    - reorganized program code
    - fixed font bug in office setup menu

--- readme.md
+++ readme.md
@@ -3,9 +3,11 @@ Small console tweaker for Windows 10 with some useful options
 
 Features:
 - add or remove desktop objects (This PC, Recycle Bin etc)
+
 - control Windows Update Center
 - control suggestions and auto completion
-- setup Microsoft Office Professional Plus 2016
-- restore SPPSvc
 
-<!-- - gpedit.msc in Windows 10 Home -->
+- setup Microsoft Office Professional Plus 2016
+- setup gpedit.msc in Windows 10 Home
+
+- restore SPPSvc
--- tenTweaker.cmd
+++ tenTweaker.cmd
@@ -66,14 +66,18 @@ echo.
 echo.
 echo.^(^>^) Choose action:
 echo.    ^(1^) Add or remove desktop objects ^(This PC, Recycle Bin etc^)
+echo.
 echo.    ^(2^) Control suggestions and auto completion
 echo.    ^(3^) Control Windows Update
+echo.
 echo.    ^(4^) Setup Microsoft Office Professional Plus 2016
-echo.    ^(5^) Restore Software Protection Platform Service ^(SPPSvc^)
+echo.    ^(5^) Setup gpedit.msc for Windows 10 Home
+echo.
+echo.    ^(6^) Restore Software Protection Platform Service ^(SPPSvc^)
 echo.
 echo.
 echo.
-choice /c 12345 /n /m "> "
+choice /c 123456 /n /m "> "
 set command=%errorLevel%
 
 
@@ -81,7 +85,8 @@ if "%command%" == "1" call :desktopObjectsMenu
 if "%command%" == "2" call :suggestionsControlMenu
 if "%command%" == "3" call :windowsUpdateControlMenu
 if "%command%" == "4" call :officeSetupMenu
-if "%command%" == "5" call :sppsvcActivatorMenu
+if "%command%" == "5" call :gpeditMSCSetupMenu
+if "%command%" == "6" call :sppsvcActivatorMenu
 
 if "%errorLevel%" == "1234567890" exit /b
 goto :mainMenu
@@ -287,7 +292,7 @@ goto :windowsUpdateControlMenu
 :officeSetupMenu
 set officeSetupURL=https://onedrive.live.com/download?cid=D3AF852448CB4BF6^&resid=D3AF852448CB4BF6%%21259^&authkey=AAK3Qw80R8to-VE
 set officeSetupAdditionalURL=https://public.dm.files.1drv.com/y4mTqNAebstFsw9p507h2xqKwivr_pHN6OwyaEAA3-xavLhFr_9HmsF-bF931oFmOZ-ynEy53Blug8XG1FLTmT0VT36kjGfbT1a_tItImyjwJqqKSTp1qCXBdPbKmlI5uNy0P6tkSMicg32ddWL3Z91nyoXV8SXymCpC_Bwp1SoqzBjBNAV4CXfr5t-QtlkJapj/Microsoft%%20Office%%20Professional%%20Plus%%202016.iso?access_token=EwAIA61DBAAUcSSzoTJJsy%%2bXrnQXgAKO5cj4yc8AAdNI1D0Km20nFjkwjZJAiQrksgJ3Bpa5AYk%%2fVPN9VGXuBitjIC6LhGh3WQcX%%2fE%%2f0V9IPo7%%2f2JLzjJnJ9%%2bSwX%%2bNm37S8I6zXYsDfy7AervE2iGE%%2bSJ901s1sjMHULB%%2btCGYvsUIEHNQTPA4dAn8gCmlrpp%%2f%%2f6cGuJnBlc2jysi1%%2bxKUcREdO8tfwpLvXaR9W%%2btDp5kKiLXvKuG9H0gCLpbknzFMkyaeeGemUTzGRglwqTTPlp94%%2fEmaMW9O5qg2STAFqKV6H%%2f%%2flNtevRIoCctJgU9dXcOfbc5YdRhySjbBGJxDLReJJk4X2zeRvq62G3ITD25jEOwYufL7POHXJOe47kDZgAACEMQTepMithw2AEdh0sQB%%2bLFCpxLdVafSfaeStp31%%2fHUPqg7TeINPS7DuEP3Ga%%2fqOPNX6CtkWzkrodHWyXsQj5eSV6ZMFZdZa2zrxSntXJs%%2bkaVAMLvGtXN8lwMXjyCZw8yhboCdwEqR8IzbgZsTR5DOXGLAcq%%2fRt81DQzUnnsHdnsuDO%%2ffELmE8ccu3eBp3ntqzz9MqxpsLotGpmwL5y72QWnmFM4UnCEhTYo1QzYoxyELtavpBik5y2%%2fSLUthnrXtxUGLuj9xAHcXfewJmGbhA3DVSnKdx9RqckzYjqBBISzqYQVmbWJeYsZIQaQrhcOkudEbpVTUplF4I%%2bYOJqiOCSI6W9lL6fTWdLuMYgsXTnnMtFMNPYeTTaYDQoZj1GqAZckKcdscy%%2b%%2foNZXkSNlPaJEZdZoozvuEFgRzt%%2fmWM9YvS7aCfia6kwDRxY9VEYwLvPQNhFpg3DGpTI%%2brrKokLUs6q9TIUBUfD1SUbXMTnN8cB1Jpsveic9wAfhg837RZVdBvfWZOYnv4myviNwqtXjgaxtzwpb6atb4EEOy6KQLAhqbZwHBdWIQhypIqFfRcATwpSENEP%%2b2hF7T878znu3rE%%2fJYijcuk%%2fH8GlzFi7y7y9%%2bl3hsW4L7eb6ybZD%%2fy7JEAI%%3d
-set officeSetupISO=Microsoft Office Professional Plus 2016.iso
+set officeSetupISO=files\microsoftOfficeProfessionalPlus2016Setup.iso
 
 call :logo
 echo.^(^i^) Microsoft Office Professional Plus 2016 Setup Menu
@@ -311,29 +316,70 @@ call :logo
 if not exist "%officeSetupISO%" (
   echo.^(^i^) Downloading Microsoft Office Professional Plus 2016 Setup
   files\wget.exe --quiet --show-progress --progress=bar:force:noscroll --no-check-certificate --tries=3 "%officeSetupURL%" --output-document="%officeSetupISO%"
-  if not exist "%officeSetupISO%" if "%officeSetupURL%" NEQ "%officeSetupAdditionalURL%" (
-    set officeSetupURL=%officeSetupAdditionalURL%
-    goto :officeSetup
-  )
   timeout /nobreak /t 1 >nul
 )
 
 echo.^(^i^) Mounting iso file...
-powershell.exe "Mount-DiskImage ""%~dp0%officeSetupISO%"""
+start /wait /b powershell.exe "Mount-DiskImage ""%~dp0%officeSetupISO%"""
 timeout /nobreak /t 1 >nul
 
-echo.^(^i^) Running setup...
+echo.^(^i^) Setup...
 for /f "skip=3" %%i in ('powershell.exe "Get-DiskImage """%~dp0%officeSetupISO%""" | Get-Volume | Select-Object {$_.DriveLetter}"') do start /wait %%i:\O16Setup.exe
 timeout /nobreak /t 1 >nul
 
 choice /c yn /n /m "(>) Setup is completed? (y/n) > "
 set command=%errorLevel%
-if "%command%" == "2" goto :officeSetup
+if "%command%" == "2" if "%officeSetupURL%" NEQ "%officeSetupAdditionalURL%" (
+  set officeSetupURL=%officeSetupAdditionalURL%
+  goto :officeSetup
+)
 
 echo.^(^i^) Unmounting iso file...
-powershell.exe "Dismount-DiskImage ""%~dp0%officeSetupISO%"""
+start /wait /b powershell.exe "Dismount-DiskImage ""%~dp0%officeSetupISO%"""
 timeout /nobreak /t 1 >nul
 goto :officeSetupMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:gpeditMSCSetupMenu
+set gpeditMSCSetup_packagesList=files\gpeditMSCSetup_packagesList.txt
+
+set gpeditMSCSetup_gpeditFile=not exist
+for /f "delims=" %%i in ('dir /a:-d /b "%winDir%\System32\gpedit.msc"') do if "%%i" == "gpedit.msc" set gpeditMSCSetup_gpeditFile=exist
+
+call :logo
+echo.^(^i^) gpedit.msc Setup Menu
+echo.
+echo.
+echo.^(^>^) Choose action:
+echo.    ^(1^) Setup ^(or repair^) gpedit.msc       %gpeditMSCSetup_gpeditFile%
+echo.
+echo.    ^(0^) Go back
+echo.
+echo.
+echo.
+choice /c 10 /n /m "> "
+set command=%errorLevel%
+
+
+if "%command%" == "2" ( set command= & exit /b )
+
+dir /b %systemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >%gpeditMSCSetup_packagesList%
+dir /b %systemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>%gpeditMSCSetup_packagesList%
+for /f %%i in ('findstr /i . %gpeditMSCSetup_packagesList% 2^>nul') do dism /online /norestart /add-package:"%systemRoot%\servicing\Packages\%%i"
+goto :gpeditMSCSetupMenu
 
 
 
