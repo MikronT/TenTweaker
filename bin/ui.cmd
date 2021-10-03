533bd7a9daba9912907355e7d95e53cf22aa6f8d
MikronT <vlad.ivanov.if@gmail.com>
2021-10-03 22:01:00 +0300

    Update logo printing

--- bin/ui.cmd
+++ bin/ui.cmd
@@ -37,19 +37,30 @@ exit /b
 
 
 :logo
-  mode con:cols=124 lines=39
   title [MikronT] %program_name%
-  color 0b
-  cls
 
-  echo.
-  echo.
-  echo.    [MikronT] ==^> %program_name%
-  echo.                  %lang_logo1%
-  echo.   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-  echo.     %lang_logo2%
-  echo.         github.com/MikronT
-  echo.
-  echo.
-  echo.
+  echo.screen_width=120
+  echo.screen_height=40
+  echo.screen_margin=1
+
+  echo.color=0b
+  echo.clear=screen
+
+
+  echo.cursor1=3 2
+  echo.text=[MikronT] ==^> %program_name%
+
+  echo.cursor1=17 3
+  echo.text=%lang_logo1%
+
+  echo.cursor1=2 4
+  echo.text=━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+  echo.cursor1=4 5
+  echo.text=%lang_logo2%
+
+  echo.cursor1=8 6
+  echo.text=github.com/MikronT
+
+  echo.cursor1=1 10
 exit /b
